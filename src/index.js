04cb07050355eb985127b0d01adcd775b67b4f9a
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2018-01-26 11:24:25 +0100

    sass-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import Articles from './components/articles';
 import registerServiceWorker from './registerServiceWorker';
 
